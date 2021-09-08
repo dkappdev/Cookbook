2c82be83c1f6349e52dd15739e327536cae73d01
Daniil <dkappdev@icloud.com>
2021-09-08 18:55:18 +0200

    Update launch screen

--- Cookbook/AppDelegate/SceneDelegate.swift
+++ Cookbook/AppDelegate/SceneDelegate.swift
@@ -20,6 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         let window = UIWindow(windowScene: windowScene)
         window.makeKeyAndVisible()
+        window.tintColor = .systemGreen
         
         let viewController = UIViewController()
         viewController.view.backgroundColor = .systemBackground
