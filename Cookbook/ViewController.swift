8b911e505fe48019ff70b72539500b1820b9aa54
Daniil <dkappdev@icloud.com>
2021-09-08 17:39:02 +0200

    Initial Commit

---
+++ Cookbook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cookbook
+//
+//  Created by Daniil Kostitsin on 08.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
