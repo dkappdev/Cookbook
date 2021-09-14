e311091967167faff99a5b44abbb69f979213ec0
Daniil <dkappdev@icloud.com>
2021-09-14 16:21:19 +0200

    Increase URL cache

--- Cookbook/AppDelegate/AppDelegate.swift
+++ Cookbook/AppDelegate/AppDelegate.swift
@@ -11,7 +11,12 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // Increasing the size of URL cache to account for the fact that we're downloading images
+        let temporaryDirectory = NSTemporaryDirectory()
+        let urlCache = URLCache(memoryCapacity: 25_000_000, diskCapacity: 50_000_000, diskPath: temporaryDirectory)
+        URLCache.shared = urlCache
+        
         return true
     }
     
