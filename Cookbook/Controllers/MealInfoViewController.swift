7174453096fa87daed475d1c8763b1a324ad1614
Daniil <dkappdev@icloud.com>
2021-09-23 19:13:32 +0300

    Create stub MealInfo view controller

---
+++ Cookbook/Controllers/MealInfoViewController.swift
@@ -0,0 +1,12 @@
+//
+//  MealInfoViewController.swift
+//  Cookbook
+//
+//  Created by Daniil Kostitsin on 20.09.2021.
+//
+
+import UIKit
+
+public class MealInfoViewController: UICollectionViewController {
+    
+}
