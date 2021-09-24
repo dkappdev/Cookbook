6452a261a85cfea1d324c360a2d793511b6e7c16
Daniil <dkappdev@icloud.com>
2021-09-24 19:44:07 +0300

    Create ingredient cell layout

--- Cookbook/Views/IngredientAmountCell.swift
+++ Cookbook/Views/IngredientAmountCell.swift
@@ -65,6 +65,61 @@ public class IngredientAmountCell: UICollectionViewCell {
     }
     
     private func setupLayout() {
+        isAccessibilityElement = true
         
+        // Ingredient amount label
+        
+        addSubview(ingredientAmountLabel)
+        ingredientAmountLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            ingredientAmountLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+            ingredientAmountLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+            ingredientAmountLabel.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+        
+        // Ingredient name label
+        
+        addSubview(ingredientNameLabel)
+        ingredientNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            ingredientNameLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+            ingredientNameLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+            ingredientNameLabel.bottomAnchor.constraint(equalTo: ingredientAmountLabel.topAnchor, constant: -8)
+        ])
+        
+        // Ingredient image view
+        
+        let containerView = UIView()
+        addSubview(containerView)
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            containerView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            containerView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            containerView.topAnchor.constraint(equalTo: topAnchor),
+            containerView.bottomAnchor.constraint(equalTo: ingredientNameLabel.topAnchor, constant: -8)
+        ])
+        
+        containerView.layer.masksToBounds = false
+        containerView.layer.shadowRadius = -6
+        containerView.layer.cornerRadius = 12
+        containerView.layer.shadowColor = UIColor.black.cgColor
+        containerView.layer.shadowOpacity = 0.2
+        containerView.layer.shadowOffset = CGSize(width: 0, height: 0)
+        
+        ingredientImageView.layer.cornerRadius = 12
+        ingredientImageView.layer.masksToBounds = true
+        
+        containerView.addSubview(ingredientImageView)
+        ingredientImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            ingredientImageView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+            ingredientImageView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+            ingredientImageView.topAnchor.constraint(equalTo: containerView.topAnchor),
+            ingredientImageView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor)
+        ])
     }
 }
