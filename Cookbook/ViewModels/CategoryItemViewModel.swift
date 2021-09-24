5f8bafb6cc9d49ea7a6a50268f36e5a868900c22
Daniil <dkappdev@icloud.com>
2021-09-24 20:04:13 +0300

    Improve category cell code structure and add more documentation

--- Cookbook/ViewModels/CategoryItemViewModel.swift
+++ Cookbook/ViewModels/CategoryItemViewModel.swift
@@ -16,10 +16,12 @@ public class CategoryItemViewModel: BaseItemViewModel {
     
     /// Cached version of downloaded image
     private var image: UIImage?
+    /// Indicates whether or not an image has already been requested
+    private var hasRequestedImage = false
+    /// Cell that most recently called `setup(_:in:at:)`. This property is used to properly set category image after receiving it from network.
+    private var mostRecentCell: CategoryCell?
     
     public let categoryInfo: CategoryInfo
-    private var hasRequestedImage = false
-    private var mostRecentCell: CategoryCell?
     
     // MARK: - Initializers
     
@@ -34,43 +36,49 @@ public class CategoryItemViewModel: BaseItemViewModel {
     public override func setup(_ cell: UICollectionReusableView, in collectionView: UICollectionView, at indexPath: IndexPath) {
         guard let cell = cell as? CategoryCell else { return }
         
-        cell.categoryNameLabel.text = categoryInfo.categoryName
-        cell.categoryImageView.image = nil
-        cell.categoryImageView.backgroundColor = .systemGray4
+        // Remembering most recent cell
+        mostRecentCell = cell
         
+        // Setting up cell's label and image view
+        cell.categoryNameLabel.text = categoryInfo.categoryName
+        
+        if let image = image {
+            cell.categoryImageView.image = image
+            cell.categoryImageView.backgroundColor = .white
+        } else {
+            cell.categoryImageView.image = nil
+            cell.categoryImageView.backgroundColor = .systemGray4
+        }
+        
+        // Setting up accessibility information
         cell.accessibilityLabel = categoryInfo.categoryName
         cell.accessibilityHint = NSLocalizedString("button_accessibility_hint", comment: "")
         
-        guard categoryInfo != CategoryInfo.empty else {
-            return
-        }
         
-        mostRecentCell = cell
+        // Requesting image only if this is not a stub cell and an image hasn't already been requested
+        guard categoryInfo != CategoryInfo.empty,
+              !hasRequestedImage else {
+                  return
+              }
         
-        if let image = image {
-            DispatchQueue.main.async {
-                cell.categoryImageView.image = image
-                cell.categoryImageView.backgroundColor = .white
-            }
-        } else if !hasRequestedImage {
-            hasRequestedImage = true
-            ArbitraryImageRequest(imageURL: categoryInfo.imageURL).send { result in
-                switch result {
-                case .success(let image):
-                    DispatchQueue.main.async {
-                        // Setting the image only if the cell was not reused
-                        if let mostRecentCell = self.mostRecentCell,
-                            collectionView.indexPath(for: mostRecentCell) == indexPath {
-                            mostRecentCell.categoryImageView.image = image
-                            mostRecentCell.categoryImageView.backgroundColor = .white
-                        }
+        hasRequestedImage = true
+        ArbitraryImageRequest(imageURL: categoryInfo.imageURL).send { result in
+            switch result {
+            case .success(let image):
+                DispatchQueue.main.async {
+                    // Setting the image only if the cell was not reused after making the request
+                    if let mostRecentCell = self.mostRecentCell,
+                       collectionView.indexPath(for: mostRecentCell) == indexPath {
+                        mostRecentCell.categoryImageView.image = image
+                        mostRecentCell.categoryImageView.backgroundColor = .white
                     }
-                    self.image = image
-                case .failure(let error):
-                    print(error)
                 }
+                self.image = image
+            case .failure(let error):
+                print(error)
             }
         }
+        
     }
     
     public override func hash(into hasher: inout Hasher) {
